81bb5ac972a541bfefe3311ebccd2ce6e813c252
Adrian Schneider <github@ardy.io>
2018-05-16 15:25:48 +1000

    move structs to separate file

---
+++ crawler/structs.go
@@ -0,0 +1,70 @@
+package crawler
+
+type Departure struct {
+    Line            *Line           `json:"-"`
+    Station         *Station        `json:"station"`
+    Destination     *Station        `json:"destination"`
+    Arrival         *time.Time      `json:"arrival"`
+    Departure       *time.Time      `json:"departure"`
+}
+
+// A train's journy across the network
+type Trip struct {
+    Departures      []*Departure    `json:"departures"`
+}
+
+// Station for multiple lines
+type Station struct {
+    Name            string          `json:"name"`
+    Id              *int            `json:"id"`
+    Departures      []*Departure    `json:"-"`
+}
+
+// Check if a station already has a departure at a specific time on a line with same destination
+func (station *Station) HasDeparture(departureTime time.Time, line *Line, destination *Station) bool {
+    _, err := station.GetDeparture(departureTime, line, destination)
+    if err == nil {
+        return true
+    }
+    return false
+}
+
+// Get Departure from station at specific time for line with destination
+func (station *Station) GetDeparture(departureTime time.Time, line *Line, destination *Station) (*Departure, error) {
+    for _, dept := range station.Departures {
+        if dept.Line == line &&
+        dept.Destination == destination &&
+        dept.Departure.Equal(departureTime) {
+            return dept, nil
+        }
+        // optimize: start from end, return false once past specifed time
+    }
+    return nil, errors.New("Station doesn't have matching departure")
+}
+
+// Line, including subtrips and different directions
+type Line struct {
+    Name            string          `json:"name"`
+    Trips           []*Trip         `json:"trips"`
+    Stops           []*Station      `json:"-"`
+}
+
+// Check if a station is one of the Termini of this line
+func (line *Line) IsTerminus(station *Station) bool {
+    // Special case for trains without line number
+    if line.Name == "U" {
+        return false
+    }
+    if station == line.Stops[0] ||
+       station == line.Stops[len(line.Stops)-1] {
+           return true
+    }
+    return false
+}
+
+
+type Network struct {
+    Provider        *goefa.EFAProvider          `json:"-"`
+    Stations        map[string]*Station         `json:"-"`
+    Lines           []*Line         `json:"lines"`
+}
